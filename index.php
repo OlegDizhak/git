6f315e8c2fa46c8846261d6842fafd2594f4ad32
OlegDizhak <olehdizhak@ukr.net>
2017-03-11 21:45:37 +0200

    Bla bla

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Class description for application class
  * Class is the main entry point in project
- * Add fix 1 commentary 111 123123213
+ * Add fix 1 commentary 111 123123213 bla bla
  **/
 class Application
 {
